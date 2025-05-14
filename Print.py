c10be4193253a7e1497e705db70597c706272347
UjaanRoy <ujaanroy2022@Gmail.com>
2025-05-14 16:00:00 +0530

    Add files via upload

---
+++ Print.py
@@ -0,0 +1,14 @@
+import keyword
+print ("Hello World!")
+print ("Welcome to coding!", 4)
+print ("\n Welcome to coding!")
+print ("Welcome to python programming",end="!")
+a=10
+print (a)
+b="codingal"
+print (b)
+c= 1.5
+print (c)
+Name=input("Put your name here:")
+print ("Hello!\n",Name,",welcome to the wonderful world of coding!")
+print (keyword.kwlist)
