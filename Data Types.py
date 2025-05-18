5236e25261d40cfdc4ef09222bd72113b5ad304d
UjaanRoy <ujaanroy2022@Gmail.com>
2025-05-18 12:30:05 +0530

    Add files via upload

---
+++ Data Types.py
@@ -0,0 +1,8 @@
+a=13
+print ("Heres the data type of a variable:",type(a))
+b=3.6
+print ("Heres the data type of b variable:",type(b))
+c="string"
+print ("Heres the data type of c variable:",type(c))
+d=True
+print ("Heres the data type of d variable:",type(d))
---
+++ TypeCasting.py
@@ -0,0 +1,12 @@
+name=input("Enter your name here:")
+print (type(name))
+age=10
+print (type (age))
+age=str(age)
+print (type (age))
+weight=46.36
+print (type (weight))
+weight=int(weight)
+print (type (weight))
+string="Welcome!"
+print (string[3:6:1])
