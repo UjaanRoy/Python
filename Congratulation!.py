6f6e7c96881c1f2c5799074e773363ab494b46fe
UjaanRoy <ujaanroy2022@Gmail.com>
2025-05-19 18:16:06 +0530

    Add files via upload

---
+++ Congratulation!.py
@@ -0,0 +1,6 @@
+# Please put the word "congratulation" due to the project story.
+print ("What ever word you put in lower caps, it will be converted upper caps!")
+print (" Please put the word 'congratulation' due to the project story.")
+Congratulations=input("Put word here:")
+Congratulations= Congratulations.upper()
+print (Congratulations)
