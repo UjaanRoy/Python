84d8ef5ee85ff1473bd00df39ddfce7306e947a0
UjaanRoy <ujaanroy2022@Gmail.com>
2025-05-25 11:53:48 +0530

    Add files via upload

---
+++ Average.py
@@ -0,0 +1,9 @@
+#There are five trees in Jack's front yard. He checks each tree to find out how tall it is in inches and writes the height on a sheet of paper. Jack's list: 98, 94, 41, 96, and 11. What is the average height of a tree in Jack's front yard?
+Tree1=98
+Tree2=94
+Tree3=41
+Tree4=96
+Tree5=11
+addition= Tree1+ Tree2+ Tree3+ Tree4+ Tree5
+average=addition/5
+print ("The average of the five tree in Jack's front yard is-",average)
