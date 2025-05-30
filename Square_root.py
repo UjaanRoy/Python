49404bd91fa39e35a27f1bb62d743f8f940ce5ab
UjaanRoy <ujaanroy2022@Gmail.com>
2025-05-30 21:15:47 +0530

    Add files via upload

---
+++ Square_root.py
@@ -0,0 +1,4 @@
+Number=int(input("Please put the number of which you want the square root of!:"))
+Square_root=Number**0.5
+#I have put ** to raise the power of the user given number to 0.5 which is the square root formula!
+print ("So the square root of",Number,"is-",Square_root)
