0e06a37625ef523fc7cd2046ec15f19ddac90260
UjaanRoy <ujaanroy2022@Gmail.com>
2025-06-01 12:18:57 +0530

    Add files via upload

---
+++ Odd or Even!.py
@@ -0,0 +1,6 @@
+print ("Type a number to see if it is ODD or EVEN!")
+Number=int(input("Enter number here:"))
+if Number % 2 ==0:
+    print ("This is an even number!")
+else:
+    print ("This is an odd number!")
