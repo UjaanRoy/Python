7f2920b4217ef0e91320b1c8d8026eb4df73f4a7
UjaanRoy <ujaanroy2022@Gmail.com>
2025-06-08 12:02:00 +0530

    Add files via upload

---
+++ Bolean.py
@@ -0,0 +1,12 @@
+Value=int(input("Put a number here:"))
+print ("Is",Value,"=3 or 4?")
+if Value==3 or Value==4:
+    print ("True")
+else:
+    print ("False")
+Value2=int(input("Put another number here to check if the number is in the 20-40 range"))
+print ("Is",Value2,">20 and <40")
+if Value2>20 and Value2<40:
+    print ("True")
+else:
+    print ("False")
