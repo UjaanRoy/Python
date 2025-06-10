bf1170c23eeb0d8fe949949d69b45f528e067af9
UjaanRoy <ujaanroy2022@Gmail.com>
2025-06-10 21:00:36 +0530

    Add files via upload

---
+++ Not equal.py
@@ -0,0 +1,15 @@
+#Write a program to check the application of not equal operator
+print ("This code checks if the two number you have entered are equals or not!")
+a=int(input("Put the first number here:"))
+b=int(input("Put the second number here:"))
+if a != b:
+    print("The two numbers are not equal!")
+else:
+    print("The two numbers are equal!")
+
+    
+
+
+
+
+
