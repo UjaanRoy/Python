cb7fc88176a736312247173c0a41f3f6277b6b12
UjaanRoy <ujaanroy2022@Gmail.com>
2025-06-22 11:18:41 +0530

    Add files via upload

---
+++ Asciii.py
@@ -0,0 +1,5 @@
+#Create a program to check the Ascii value of a given character.
+Number=(input("put a on digit number here to see ASCII value."))
+if len(Number)==1:
+    ASCII=ord(Number)
+    print (ASCII)
