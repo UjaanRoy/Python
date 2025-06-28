028de0a29d9ddca000de905ff6ecd86eb860b9c0
UjaanRoy <ujaanroy2022@Gmail.com>
2025-06-28 22:17:20 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,16 @@
+"""Tiya is solving a mathematical problem. But she has mistakenly interchanged 3 of the values. So now
+she needs to place them in their correct position. So design a program for swapping 3 values.
+Best of luck."""
+a=5
+b=8
+c=3
+print ("before swapping")
+prob=(a+b)**c
+print (prob)
+#For swapping the numbers I would just simply manually assign the variables in this way-
+a=c
+b=a
+c=b
+print ("after swapping")
+prob=(a+b)**c
+print (prob)
