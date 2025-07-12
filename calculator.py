83dc155172c868c68737e4e7b29c911d8ed8bb69
UjaanRoy <211190005+UjaanRoy@users.noreply.github.com>
2025-07-12 16:48:58 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,11 @@
+"""Vrisha is very good at maths. But her friends always get stuck whenever
+there is power calculation. They feel it is too lengthy, and too much
+calculation is required to solve the problem. So, to help her friends, she
+designed a program where users can calculate n number of power of the
+given number.
+"""
+print ("Using this code you can get the power of a number easily")
+number=int(input("Put number:"))
+power=int(input("Value to be raised to (to the power of):"))
+output=(number**power)
+print (number,"raised to the power of",power,"is",output)
